4fcd922f78c11a0300e4e8d102fb02a3bb12c606
J.R. Walker <rosswalk@gmail.com>
2016-01-20 19:25:58 -0500

    - initial commit

---
+++ sql/timesheet.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2016 at 01:22 AM
+-- Server version: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `timesheet`
+--
+CREATE DATABASE IF NOT EXISTS `timesheet` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `timesheet`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+CREATE TABLE `team` (
+  `team_number` int(11) NOT NULL,
+  `team_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timelog`
+--
+
+DROP TABLE IF EXISTS `timelog`;
+CREATE TABLE `timelog` (
+  `timelog_id` int(11) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `timesheet_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` varchar(20) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `team_number` int(11) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `user_password` varchar(32) NOT NULL,
+  `user_admin` tinyint(1) NOT NULL,
+  `user_mentor` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`team_number`),
+  ADD UNIQUE KEY `team_name` (`team_name`);
+
+--
+-- Indexes for table `timelog`
+--
+ALTER TABLE `timelog`
+  ADD PRIMARY KEY (`timelog_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `team_number` (`team_number`),
+  ADD KEY `user_email` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `timelog`
+--
+ALTER TABLE `timelog`
+  MODIFY `timelog_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `timelog`
+--
+ALTER TABLE `timelog`
+  ADD CONSTRAINT `timelog_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`team_number`) REFERENCES `team` (`team_number`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
